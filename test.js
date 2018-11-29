a38dd98e70e7423950a6c71435c21e8d3ccb7a4a
Jacek Kopecky <jacek@jacek.cz>
2018-11-29 16:52:07 +0000

    :arrow_up: move to qunit 2.8

--- test.js
+++ test.js
@@ -3,12 +3,11 @@
 const fs = require('fs');
 const mysql = require('mysql2/promise');
 
-
 /**
  * In the `worksheet` folder, make a module called `cardb.js`
  * that implements a simple database of cars, as defined below.
  *
- * The module exports two asynchronous functions:
+ * The module exports three asynchronous functions:
  *  1. `saveCar` with the following parameters: `reg`, `make`,
  *     `model`, `year`, and `price`. The function stores
  *     the information in a suitable database table
@@ -18,6 +17,11 @@ const mysql = require('mysql2/promise');
  *     The function should return the average price of all known cars
  *     from the given year. In case of an error (such as that we don't
  *     have any cars from the given year) it should return `null`.
+ *
+ *  3. `shutdown` with no parameters. The function closes any connection(s)
+ *     used by the module. Use `await connection.end()` – substitute the
+ *     word `connection` with the name of your own variable initialized
+ *     with the call to `mysql.createConnection()`.
  *
  * Put your database initialization SQL code in `worksheet/cardb_init.sql`.
  * Run the database initialization code in your MySQL so your database is ready for testing.
@@ -33,144 +37,167 @@ const mysql = require('mysql2/promise');
  * or a JS script that puts in some testing data.
  */
 
-/* global test, ok, expect, stop, start, equal, strictEqual */
+/* global QUnit */
 /* eslint-disable no-restricted-globals */
 
 
-test(
+QUnit.test(
   'Create a file `worksheet/config.json`',
-  () => {
+  (assert) => {
     try {
       fs.accessSync('./worksheet/config.json', fs.F_OK);
-      ok(true, '`worksheet/config.json` created');
+      assert.ok(true, '`worksheet/config.json` created');
     } catch (e) {
-      ok(false, '`worksheet/config.json` is missing - please create it');
+      assert.ok(false, '`worksheet/config.json` is missing - please create it');
     }
 
     const config = require('./worksheet/config.json'); // eslint-disable-line global-require
-    ok(config.mysql.table, '`worksheet/config.json` needs a property `table` within `mysql`');
+    assert.ok(config.mysql.table, '`worksheet/config.json` needs a property `table` within `mysql`');
   },
 );
 
 
-test(
+QUnit.test(
   'Create a file `worksheet/cardb_init.sql`',
-  () => {
+  (assert) => {
     try {
       fs.accessSync('./worksheet/cardb_init.sql', fs.F_OK);
-      ok(true, '`worksheet/cardb_init.sql` created');
+      assert.ok(true, '`worksheet/cardb_init.sql` created');
     } catch (e) {
-      ok(false, '`worksheet/cardb_init.sql` is missing - please create it');
+      assert.ok(false, '`worksheet/cardb_init.sql` is missing - please create it');
     }
   },
 );
 
 
-test(
+QUnit.test(
   'Create a file `worksheet/cardb.js`',
-  () => {
+  (assert) => {
     try {
       fs.accessSync('./worksheet/cardb.js', fs.F_OK);
-      ok(true, '`worksheet/cardb.js` created');
+      assert.ok(true, '`worksheet/cardb.js` created');
     } catch (e) {
-      ok(false, '`worksheet/cardb.js` is missing - please create it');
+      assert.ok(false, '`worksheet/cardb.js` is missing - please create it');
     }
 
     const cardb = require('./worksheet/cardb'); // eslint-disable-line global-require
 
-    ok(typeof cardb.saveCar === 'function', 'cardb.js must export a function called `saveCar`');
-    ok(typeof cardb.getAveragePrice === 'function', 'cardb.js must export a function called `getAveragePrice`');
+    assert.ok(typeof cardb.saveCar === 'function', 'cardb.js must export a function called `saveCar`');
+    assert.ok(typeof cardb.getAveragePrice === 'function', 'cardb.js must export a function called `getAveragePrice`');
   },
 );
 
 
-test(
+QUnit.test(
   'Clear the database',
-  async () => {
+  async (assert) => {
     const config = require('./worksheet/config.json'); // eslint-disable-line global-require
 
-    expect(2);
-    stop();
+    assert.expect(2);
+    const done = assert.async();
     try {
       const sql = await mysql.createConnection(config.mysql);
       const [rows] = await sql.query(sql.format('select count(*) from ??', [config.mysql.table]));
-      ok('count(*)' in rows[0], 'Table ' + config.mysql.table + ' exists');
+      assert.ok('count(*)' in rows[0], 'Table ' + config.mysql.table + ' exists');
       await sql.query(sql.format('delete from ??', [config.mysql.table]));
-      ok(true, 'Table ' + config.mysql.table + ' cleared');
+      assert.ok(true, 'Table ' + config.mysql.table + ' cleared');
+      await sql.end();
     } catch (e) {
       console.error(e);
-      ok(false, 'SQL error clearing table ' + config.mysql.table + ': ' + e);
+      assert.ok(false, 'SQL error clearing table ' + config.mysql.table + ': ' + e);
     }
-    start();
+    done();
   },
 );
 
 
-test(
+QUnit.test(
   'saveCar',
-  async () => {
+  async (assert) => {
     const cardb = require('./worksheet/cardb'); // eslint-disable-line global-require
     const config = require('./worksheet/config.json'); // eslint-disable-line global-require
 
-    expect(3);
-    stop();
+    assert.expect(3);
+    const done = assert.async();
     try {
       const sql = await mysql.createConnection(config.mysql);
       let rows;
 
       await cardb.saveCar('han 5010', 'Ford', 'Harrison', 1980, 8999.99);
       [rows] = await sql.query(sql.format('select count(*) from ??', [config.mysql.table]));
-      equal(rows[0]['count(*)'], 1, 'Expecting one car in the table');
+      assert.equal(rows[0]['count(*)'], 1, 'Expecting one car in the table');
 
       // beware sql injection with spurious " and '
       await cardb.saveCar('bn18 qqq"', 'Brand', 'New', 2018, 15000);
       [rows] = await sql.query(sql.format('select count(*) from ??', [config.mysql.table]));
-      equal(rows[0]['count(*)'], 2, 'Expecting two cars in the table');
+      assert.equal(rows[0]['count(*)'], 2, 'Expecting two cars in the table');
 
       await cardb.saveCar('abcd efg', 'Luxurius', 'Novus\'', 2018, 47000);
       [rows] = await sql.query(sql.format('select count(*) from ??', [config.mysql.table]));
-      equal(rows[0]['count(*)'], 3, 'Expecting three cars in the table');
+      assert.equal(rows[0]['count(*)'], 3, 'Expecting three cars in the table');
+      await sql.end();
     } catch (e) {
       console.error(e);
-      ok(false, 'Error saving car: ' + e);
+      assert.ok(false, 'Error saving car: ' + e);
     }
 
-    start();
+    done();
   },
 );
 
 
-test(
+QUnit.test(
   'getAveragePrice',
-  async () => {
+  async (assert) => {
     // test average for one car
     // test average for two cars
     // test average for nonexistent year
     const cardb = require('./worksheet/cardb'); // eslint-disable-line global-require
 
-    expect(5);
-    stop();
+    assert.expect(5);
+    const done = assert.async();
 
     try {
       let avg = await cardb.getAveragePrice(1980);
-      strictEqual(avg, 8999.99, 'the average price of cars from 1980 is 8999.99');
+      assert.strictEqual(avg, 8999.99, 'the average price of cars from 1980 is 8999.99');
 
       avg = await cardb.getAveragePrice(2018);
-      strictEqual(avg, 31000, 'the average price of cars from 2018 is 31k');
+      assert.strictEqual(avg, 31000, 'the average price of cars from 2018 is 31k');
 
       avg = await cardb.getAveragePrice(3000);
-      strictEqual(avg, null, 'no cars from 3000, the call should return null');
+      assert.strictEqual(avg, null, 'no cars from 3000, the call should return null');
 
       avg = await cardb.getAveragePrice('\'30');
-      strictEqual(avg, null, 'no cars from \'30 - beware sql injection, the call should return null');
+      assert.strictEqual(avg, null, 'no cars from \'30 - beware sql injection, the call should return null');
 
       avg = await cardb.getAveragePrice('"30');
-      strictEqual(avg, null, 'no cars from "30 - beware sql injection, the call should return null');
+      assert.strictEqual(avg, null, 'no cars from "30 - beware sql injection, the call should return null');
     } catch (e) {
       console.error(e);
-      ok(false, 'Error getting averages: ' + e);
+      assert.ok(false, 'Error getting averages: ' + e);
     }
 
-    start();
+    done();
+  },
+);
+
+
+QUnit.test(
+  'close db connection',
+  async (assert) => {
+    const cardb = require('./worksheet/cardb'); // eslint-disable-line global-require
+
+    assert.expect(1);
+    const done = assert.async();
+
+    try {
+      await cardb.shutdown();
+      assert.ok(true, 'if connection is not shut down correctly, the test will not finish');
+    } catch (e) {
+      console.error(e);
+      assert.ok(false, 'Error getting averages: ' + e);
+    }
+
+    done();
   },
 );
